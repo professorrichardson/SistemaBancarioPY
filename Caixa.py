23178b455ec5a70b0ebb67091f8f261d9f87267f
dudd44 <luciano.eduarda@escola.pr.gov.br>
2026-03-06 09:55:21 -0300

    commit 3

--- Caixa.py
+++ Caixa.py
@@ -4,6 +4,13 @@
 # Turma: 3ºTec
 # ==========================================
 
+#Cores no terminal PY
+VERMELHO = "\033[31m"
+VERDE = "\033[032m"
+AMARELO = "\033[033m"
+AZUL = "\033[034m"
+RESET = "\033[0m"
+
 print("==========================================")
 print("======SIMULADOR DE CAIXA ELETRÔNICO=======")
 print("==========================================")
@@ -113,35 +120,38 @@ def ver_extrato():
 # ----------------------------
 def main():
 
-    while True:
-        exibir_menu()
-        opcao = input("Escolha uma opção: ")
+     while True:
+      exibir_menu()
+      opcao_str = input("Escolha uma opção: ")
+      if not opcao_str.isdigit():
+          print(VERMELHO+"Opção inválida, digite apenas números!"+RESET)
+          continue
+     
 
-        # TODO 12:
-        # Validar se a opção é numérica.
+      # TODO 12:
+      # Validar se a opção é numérica.
 
-        opcao = int(opcao)
+      opcao = int(opcao_str)
 
-        if opcao == 1:
-            consultar_saldo()
+      if opcao == 1:
+          consultar_saldo()
 
-        elif opcao == 2:
-            depositar()
+      elif opcao == 2:
+          depositar()
 
-        elif opcao == 3:
-            sacar()
+      elif opcao == 3:
+          sacar()
 
-        elif opcao == 4:
-            ver_extrato()
+      elif opcao == 4:
+          ver_extrato()
 
-        elif opcao == 5:
-            print("Encerrando sistema...")
-            break
+      elif opcao == 5:
+          print("Encerrando sistema...")
+          break
 
-        else:
-            print("Opção inválida!")
-
-
+      else:
+          print(AMARELO+"Escolha apenas 1, 2, 3, 4 ou 5"+RESET)
+          
 # ----------------------------
 # EXECUÇÃO DO SISTEMA
 # ----------------------------
